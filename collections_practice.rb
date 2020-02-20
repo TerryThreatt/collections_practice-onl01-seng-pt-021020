c0d008cbb5ab0cf2537b842d80879f14c22d99cd
Terry Threatt <terry.threatt@gmail.com>
2020-02-20 14:48:05 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -34,15 +34,6 @@ def find_a(array)
 end
 
 
-# def sum_array(array)
-#   sum = 0
-#   array.each do |num|
-#     sum+=num
-#   end
-#   sum
-# end 
-# end
-
 # def add_s(array)
 #   array.collect do |word|
 #     if array[1] == word
